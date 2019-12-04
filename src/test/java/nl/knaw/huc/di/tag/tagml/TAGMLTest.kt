6b04285226b5111fd4aca9d9d884a41233dfae80
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-12-04 17:38:56 +0100

    [TRD-478] Add parse tests

--- src/test/java/nl/knaw/huc/di/tag/tagml/TAGMLTest.kt
+++ src/test/java/nl/knaw/huc/di/tag/tagml/TAGMLTest.kt
@@ -19,21 +19,35 @@
  */
 package nl.knaw.huc.di.tag.tagml
 
+import arrow.core.Either
+import arrow.core.Left
+import arrow.core.Right
 import nl.knaw.huc.di.tag.tagml.TAGML.escapeDoubleQuotedText
 import nl.knaw.huc.di.tag.tagml.TAGML.escapeRegularText
 import nl.knaw.huc.di.tag.tagml.TAGML.escapeSingleQuotedText
 import nl.knaw.huc.di.tag.tagml.TAGML.escapeVariantText
 import nl.knaw.huc.di.tag.tagml.TAGML.unEscape
-import org.assertj.core.api.Assertions
+import nl.knaw.huc.di.tag.tagml.grammar.TAGMLLexer
+import nl.knaw.huc.di.tag.tagml.grammar.TAGMLParser
+import org.antlr.v4.runtime.*
+import org.antlr.v4.runtime.atn.ATNConfigSet
+import org.antlr.v4.runtime.dfa.DFA
+import org.antlr.v4.runtime.tree.ParseTree
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.*
+
 
 class TAGMLTest {
+
     @Test
     fun testEscapeRegularText() {
         val text = """Escape these characters: \ < [, but not these: | " ' """
         val expectation = """Escape these characters: \\ \< \[, but not these: | " ' """
         val escaped = escapeRegularText(text)
-        Assertions.assertThat(escaped).isEqualTo(expectation)
+        assertThat(escaped).isEqualTo(expectation)
     }
 
     @Test
@@ -41,7 +55,7 @@ class TAGMLTest {
         val text = """Escape these characters : \ < [|, but not these: " ' """
         val expectation = """Escape these characters : \\ \< \[\|, but not these: " ' """
         val escaped = escapeVariantText(text)
-        Assertions.assertThat(escaped).isEqualTo(expectation)
+        assertThat(escaped).isEqualTo(expectation)
     }
 
     @Test
@@ -49,7 +63,7 @@ class TAGMLTest {
         val text = """Escape these characters: \ ', but not these: < [ | " """
         val expectation = """Escape these characters: \\ \', but not these: < [ | " """
         val escaped = escapeSingleQuotedText(text)
-        Assertions.assertThat(escaped).isEqualTo(expectation)
+        assertThat(escaped).isEqualTo(expectation)
     }
 
     @Test
@@ -57,7 +71,7 @@ class TAGMLTest {
         val text = """Escape these characters: \ ", but not these: < [ | ' """
         val expectation = """Escape these characters: \\ \", but not these: < [ | ' """
         val escaped = escapeDoubleQuotedText(text)
-        Assertions.assertThat(escaped).isEqualTo(expectation)
+        assertThat(escaped).isEqualTo(expectation)
     }
 
     @Test
@@ -65,6 +79,106 @@ class TAGMLTest {
         val text = """Unescape this: \< \[ \| \! \" \' \\ """
         val expectation = """Unescape this: < [ | ! " ' \ """
         val unEscaped = unEscape(text)
-        Assertions.assertThat(unEscaped).isEqualTo(expectation)
+        assertThat(unEscaped).isEqualTo(expectation)
     }
+
+    @Test
+    fun testCorrectTAGML() {
+        val tagml = "[!schema http://alexandria.net/schemas/xyz.yaml]\n[tagml>Hello World!<tagml]\n"
+        assertParseSucceeds(tagml)
+    }
+
+    private fun assertParseSucceeds(tagml: String) {
+        val result = parse(tagml)
+        assertThat(result is Either.Right)
+    }
+
+    private fun assertParseFails(tagml: String) {
+        val result = parse(tagml)
+        assertThat(result is Either.Left)
+    }
+
+    @Test
+    fun testIncorrectTAGML() {
+        val tagml = "<xml>This is not TAGML!</xml>"
+        assertParseFails(tagml)
+    }
+
+    val LOG: Logger = LoggerFactory.getLogger(TAGMLTest::class.java)
+
+    fun parse(tagml: String): Either<List<String>, ParseTree> {
+        val antlrInputStream: CharStream = CharStreams.fromString(tagml)
+        val lexer = TAGMLLexer(antlrInputStream)
+        val errorListener = TestErrorListener()
+        lexer.addErrorListener(errorListener)
+        val tokens = CommonTokenStream(lexer)
+
+        val parser = TAGMLParser(tokens)
+        parser.addErrorListener(errorListener)
+        parser.buildParseTree = true
+
+        val parseTree: ParseTree = parser.document()
+//    val listener = TestListener(errorListener)
+//    ParseTreeWalker.DEFAULT.walk(listener, parseTree)
+        return if (errorListener.hasErrors()) {
+            Left(errorListener.errors)
+        } else {
+            Right(parseTree)
+        }
+    }
+}
+
+class TestErrorListener : ANTLRErrorListener {
+    private val LOG: Logger = LoggerFactory.getLogger(TestErrorListener::class.java)
+    internal val errors: MutableList<String> = ArrayList()
+    private val reportAmbiguity = false
+    private val reportAttemptingFullContext = false
+    private val reportContextSensitivity = true
+
+    override fun syntaxError(recognizer: Recognizer<*, *>?, offendingSymbol: Any?, line: Int, charPositionInLine: Int, msg: String, e: RecognitionException?) {
+        errors.add(String.format("syntax error: line %d:%d %s", line, charPositionInLine, msg.replace("token recognition error at", "unexpected token")))
+    }
+
+    override fun reportAmbiguity(recognizer: Parser, dfa: DFA, startIndex: Int, stopIndex: Int, exact: Boolean, ambigAlts: BitSet, configs: ATNConfigSet) {
+        if (reportAmbiguity) {
+            errors.add("ambiguity:\n recognizer=" + recognizer //
+                    + ",\n dfa=" + dfa //
+                    + ",\n startIndex=" + startIndex //
+                    + ",\n stopIndex=" + stopIndex //
+                    + ",\n exact=" + exact //
+                    + ",\n ambigAlts=" + ambigAlts //
+                    + ",\n configs=" + configs)
+        }
+    }
+
+    override fun reportAttemptingFullContext(recognizer: Parser, dfa: DFA, startIndex: Int, stopIndex: Int, conflictingAlts: BitSet, configs: ATNConfigSet) {
+        if (reportAttemptingFullContext) {
+            errors.add("attempting full context error:\n recognizer=" + recognizer //
+                    + ",\n dfa=" + dfa //
+                    + ",\n startIndex=" + startIndex //
+                    + ",\n stopIndex=" + stopIndex //
+                    + ",\n conflictingAlts=" + conflictingAlts //
+                    + ",\n configs=" + configs)
+        }
+    }
+
+    override fun reportContextSensitivity(recognizer: Parser, dfa: DFA, startIndex: Int, stopIndex: Int, prediction: Int, configs: ATNConfigSet) {
+        if (reportContextSensitivity) {
+            errors.add("context sensitivity error:\n recognizer=" + recognizer //
+                    + ",\n dfa=" + dfa //
+                    + ",\n startIndex=" + startIndex //
+                    + ",\n stopIndex=" + stopIndex //
+                    + ",\n prediction=" + prediction //
+                    + ",\n configs=" + configs)
+        }
+    }
+
+    fun hasErrors(): Boolean {
+        return !errors.isEmpty()
+    }
+
+    fun addError(messageTemplate: String?, vararg messageArgs: Any?) {
+        errors.add(String.format(messageTemplate!!, *messageArgs))
+    }
+
 }
