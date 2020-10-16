403ac925d00e4664fd09b9d71c1cd867ffb30d80
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2020-10-16 16:58:58 +0200

    fix copyright header

--- src/test/kotlin/nl/knaw/huc/di/tag/ANTLRUtils.kt
+++ src/test/kotlin/nl/knaw/huc/di/tag/ANTLRUtils.kt
@@ -1,5 +1,25 @@
 package nl.knaw.huc.di.tag
 
+/*-
+ * #%L
+ * tagml
+ * =======
+ * Copyright (C) 2016 - 2020 HuC DI (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import de.vandermeer.asciitable.AsciiTable
 import de.vandermeer.asciitable.CWC_LongestLine
 import de.vandermeer.asciithemes.a7.A7_Grids
@@ -34,26 +54,7 @@ object ANTLRUtils {
             token = lexer.nextToken()
             //      LOG.info(token.toString());
 
-/*-
- * #%L
- * tagml
- * =======
- * Copyright (C) 2016 - 2020 HuC DI (KNAW)
- * =======
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-           if (token.type != Token.EOF) {
+            if (token.type != Token.EOF) {
                 val pos = token.line.toString() + ":" + token.charPositionInLine
                 val text = "'" + token.text + "'"
                 val rule = lexer.ruleNames[token.type - 1]
